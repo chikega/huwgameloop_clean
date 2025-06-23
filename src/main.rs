80ef03aee040200dfe3424507a0d9c6d989bb1ee
Gary Chike <chikega@pm.me>
2025-06-23 10:15:08 -0400

    Did away w/ if-else block and created two if statements instead, added 'continue' back in

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 // Edited 08/02/2023   reviewing with Git, added else if, else 
 // Edited 03/02/2024   added sleep, Duration, thread::sleep
 // Edited 09/22/2024   added const Q, to_uppercase()
+// Edited: 06/23/2025  did away w/ if-else block and created
+// two if statements instead, added 'continue' back in
 
 use std::io::{stdin, stdout, Write}; // 03/02/2024
 
@@ -10,7 +12,7 @@ use std::time::Duration;
 use std::thread::sleep;
 
 fn main() {
-    const Q: &str = "Q"; // explicit type annotation with const
+    const Q: &str = "Q"; // explicit type annotation required with 'const'
     
     println!("> press <Q> to exit..");
     loop {
@@ -19,9 +21,9 @@ fn main() {
         stdin()
             .read_line(&mut input)
                 .expect("> Failed to read line");
-        if input.trim().is_empty() {} // 08/02/2023 deleted 'continue', Rust expects a 'block' - {}
-        else if input.trim().to_uppercase() == Q {break;} // 09/22/2024 changed to_uppercase() and added Q
-        else {println!("> You wrote '{}'", input.trim() );}
+        if input.trim().is_empty() {continue;} // 08/02/2023 deleted 'continue', Rust expects at minimum an empty 'block' - {}
+        if input.trim().to_uppercase() == Q {break;} // 09/22/2024 changed to_uppercase() and added Q
+        println!("> You wrote '{}'", input.trim() );
     }
     println!("> Goodbye!");
     sleep(Duration::from_secs(2));
